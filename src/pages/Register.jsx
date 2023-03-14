63dea5fc7b872fbf27d3ded143fbd6f28252e83c
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-03-14 21:10:55 +0300

    import and state created

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import GoogleIcon from "../assets/icons/GoogleIcon";
+import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { createUser, signUpProvider } = useContext(AuthContext);
+
   return (
     <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
       <div className={`form-container mt-[5vh] w-[380px] h-[580px]`}>
