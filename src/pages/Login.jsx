a0f0d8088ed5db3b5866813b8a25727d46737746
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-03-14 21:03:04 +0300

    link,googleıcon,auhcontext import

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import GoogleIcon from "../assets/icons/GoogleIcon";
+import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
   return <div>Login</div>;
