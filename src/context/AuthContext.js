239c2fdc7dd1273cdd1c1fe8c07f3003f1a8af39
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-03-14 20:39:46 +0300

    authcontext added

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,13 @@
-import React, { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../auth/firebase";
+import { toastErrorNotify, toastSuccessNotify } from "../helpers/ToastNotify";
 
 // export const {Provider} = createContext()
 export const AuthContext = createContext();
@@ -8,7 +17,64 @@ export const AuthContext = createContext();
 //   };
 
 const AuthContextProvider = ({ children }) => {
-  const values = { currentUser: { displayName: "felix franko" } };
+  const [currentUser, setCurrentUser] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    userObserver();
+  }, []);
+
+  const createUser = async (email, password) => {
+    try {
+      //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+      let userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      navigate("/");
+      toastSuccessNotify("Registered successfully!");
+    } catch (error) {
+      toastErrorNotify(error.message);
+    }
+  };
+
+  //* https://console.firebase.google.com/
+  //* => Authentication => sign-in-method => enable Email/password
+  //! Email/password ile girişi enable yap
+  const signIn = async (email, password) => {
+    //? mevcut kullanıcının giriş yapması için kullanılan firebase metodu
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+      toastSuccessNotify("Logged in successfully!");
+    } catch (error) {
+      toastErrorNotify(error.message);
+    }
+  };
+
+  const logOut = () => {
+    signOut(auth);
+  };
+
+  const userObserver = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { email, displayName, photoURL } = user;
+        setCurrentUser({ email, displayName, photoURL });
+      } else {
+        setCurrentUser(false);
+        // console.log("logged out");
+      }
+    });
+  };
+
+  const values = {
+    createUser,
+    signIn,
+    logOut,
+    currentUser,
+  };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
